6f5cce22a02880865ce91a08949f0e2a54aba3e5
h3nrey <pedro.novaes.education@gmail.com>
2023-10-14 12:54:50 -0300

    feat: adding tries system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,16 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Utils;
+using NaughtyAttributes;
 
 public class PlayerController : MonoBehaviour {
     public static PlayerController instance;
+    public Player data;
+
+    #region Events
 
     // Events
     public UnityEvent<Vector2> onMove;
 
-    // Properties
+    public UnityEvent onDie;
+
+    #endregion Events
+
+    #region Properties
+
+    // Tries
+    [ReadOnly]
+    public int currentTries;
+
+    // Movement
     [SerializeField]
     private float speed;
+
+    [Header("Initialization")]
+    [SerializeField]
+    private Transform startPoint;
 
     // Log
     [Header("Log Collision")]
@@ -34,12 +53,18 @@ public class PlayerController : MonoBehaviour {
 
     // Flags
     [Header("Flags")]
+    public bool isAlive;
+
     public bool isTouchingPlataform;
 
     // Components
     [Header("Components")]
     [SerializeField]
     private Rigidbody2D rb;
+
+    #endregion Properties
+
+    #region Defaul Methods
 
     private void Awake() {
         if (instance == null) {
@@ -48,21 +73,37 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void Start() {
+        isAlive = true;
+        currentTries = data.tries;
         onMove.AddListener((Vector2 dir) => Move(dir));
+        onDie.AddListener(Die);
+    }
+
+    private void OnDestroy() {
+        onMove.RemoveAllListeners();
+        onDie.RemoveAllListeners();
     }
 
     private void FixedUpdate() {
         CheckLogCollision();
+        CheckLakeCollision();
     }
+
+    #endregion Defaul Methods
+
+    #region Movement
 
     public void Move(Vector2 dir) {
         Vector2 pos = rb.position;
         rb.MovePosition(new Vector2(pos.x + (dir.x * speed), pos.y + (dir.y * speed)));
     }
 
+    #endregion Movement
+
+    #region Collision
+
     public void CheckLogCollision() {
         Collider2D[] touchingPlataforms = Physics2D.OverlapCircleAll(plataformPoint.position, logRange, plataformLayer);
-        bool touchingLake = Physics2D.OverlapCircle(plataformPoint.position, lakeRange, lakeLayer);
 
         if (touchingPlataforms.Length > 0) {
             isTouchingPlataform = true;
@@ -73,18 +114,31 @@ public class PlayerController : MonoBehaviour {
             isTouchingPlataform = false;
             transform.SetParent(null);
         }
+    }
 
-        if (touchingLake && touchingPlataforms.Length < 1) {
-            print("Touching lake");
+    public void CheckLakeCollision() {
+        bool touchingLake = Physics2D.OverlapCircle(plataformPoint.position, lakeRange, lakeLayer);
+
+        if (touchingLake && !isTouchingPlataform) {
+            onDie?.Invoke();
         }
     }
 
-    //public void OnTriggerStay2D(Collider2D other) {
-    //    GameObject otherObj = other.gameObject;
-    //    if (otherObj.CompareTag("lake") && !isTouchingPlataform) {
-    //        print("Touching lake");
-    //    }
-    //}
+    #endregion Collision
+
+    private void Die() {
+        if (isAlive == false) return;
+        isAlive = false;
+        print("died");
+
+        currentTries -= 1;
+        Coroutines.DoAfter(() => {
+            transform.position = startPoint.position;
+            isAlive = true;
+        }, 0.5f, this);
+    }
+
+    #region Gizmos
 
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
@@ -92,4 +146,6 @@ public class PlayerController : MonoBehaviour {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(plataformPoint.position, lakeRange);
     }
+
+    #endregion Gizmos
 }
